805852f41bc5090d2f2e50860b7fc920617acc0a
Niloy Sarker <niloysarker96@gmail.com>
2022-02-10 19:02:34 +0100

    Working on Date Time Encoder

--- ImageClassification/DateTimeEncoder/DatetimeEncoder.cs
+++ ImageClassification/DateTimeEncoder/DatetimeEncoder.cs
@@ -1,7 +1,8 @@
-﻿
+﻿// Copyright (c) Damir Dobric. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using System;
 using System.Collections.Generic;
-
+using System.Reflection.Metadata.Ecma335;
 
 namespace NeoCortexApi.Encoders
 {
