c7a80fbeebbe224f71c5129aae0ca8d4325dd0b8
Niloy Sarker <niloysarker96@gmail.com>
2022-03-13 13:00:57 +0100

    Updated the overall code & updated project structure.

---
+++ MyProject/ImageClassification/ArgsConfig/ArgsConfig.cs
@@ -0,0 +1,78 @@
+﻿using NeoCortexApi.Entities;
+using Newtonsoft.Json;
+
+namespace AConfig
+{
+    public class ArgsConfig
+    {
+        private List<string> listInputWithValue = new() {"-cf","-if" };
+        public HtmConfig htmConfig;
+        public string inputFolder;
+        public string configFile;
+        public string saveFormat;
+        public bool ifSaveResult;
+        public string saveResultPath;
+
+        public ArgsConfig(string[] args)
+        {
+            configFile = "";
+            //Parsing the input cmd
+            int index = 0;
+            string currentDir = Directory.GetCurrentDirectory();
+            while (index < args.Length)
+            {
+                if (listInputWithValue.Contains(args[index]))
+                {
+                    switch (args[index])
+                    {
+                        case "-if":
+                            // current input InputFolder
+                            index += 1;
+                            string root = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+                            //string path = Path.Combine(root);
+                            inputFolder = Path.Combine(root, args[index]);
+                            break;
+                        case "-cf":
+                            // current config file htmconfig1.json
+                            index += 1;
+                            configFile = Path.Combine(currentDir, args[index]);
+                            break;
+                        case "--save-format":
+                            index+=1;
+                            saveFormat = args[index];
+                            break;
+                        case "--save-result":
+                            ifSaveResult = true;
+                            break;
+                        case "--save-result-path":
+                            index+=1;
+                            saveResultPath = args[index];
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                index+=1;
+            }
+            // Adding htmconfig1.json to a Dictionary
+            htmConfig = SetupHtmConfigParameters(configFile);
+        }
+
+        
+        public HtmConfig SetupHtmConfigParameters(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("File Name is empty ");
+            }
+            using (StreamReader sw = new StreamReader(fileName))
+            {
+                var cfgJson = sw.ReadToEnd();
+                JsonSerializerSettings settings1 = new JsonSerializerSettings { Formatting = Formatting.Indented };
+                HtmConfig htmConfig = JsonConvert.DeserializeObject<HtmConfig>(cfgJson, settings1);
+                //htmConfig.Random = new Random(42);
+                return htmConfig;
+            }
+        }
+    }
+}
---
+++ MyProject/ImageClassification/ImageClassification/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp;
+using AConfig;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Network;
+using NeoCortexApi.Utility;
+
+
+void execute(string inp,string monthName,int idx)
+{
+    CortexNetworkContext ctx = new CortexNetworkContext();
+    DateTimeOffset now = DateTimeOffset.Now;
+    Dictionary<string, Dictionary<string, object>> encoderSettings = new Dictionary<string, Dictionary<string, object>>();
+    encoderSettings.Add("DateTimeEncoder", new Dictionary<string, object>()
+        {
+            { "W", 21},
+            { "N", 1024},
+            { "MinVal", now.AddYears(-20)},
+            { "MaxVal", now},
+            { "Periodic", false},
+            { "Name", "DateTimeEncoder"},
+            { "ClipInput", false},
+            { "Padding", 5},
+        });
+    DateTimeEncoder encoder = new DateTimeEncoder(encoderSettings, DateTimeEncoder.Precision.Days);
+    int[] result = encoder.Encode(DateTimeOffset.Parse(inp.ToString()));
+    //Debug.WriteLine(NeoCortexApi.Helpers.StringifyVector(result));
+    //Debug.WriteLine(NeoCortexApi.Helpers.StringifyVector(expectedOutput));
+    int[,] twoDimenArray = ArrayUtils.Make2DArray<int>(result, 32, 32);
+    int[,] twoDimArray = ArrayUtils.Transpose(twoDimenArray);
+    NeoCortex.NeoCortexUtils.DrawBitmap(twoDimArray, 1024, 1024, $"DateTime_out_{inp.ToString().Replace("/", "-").Replace(":", "-")}_32x32-N-{encoderSettings["DateTimeEncoder"]["N"]}-W-{encoderSettings["DateTimeEncoder"]["W"]}.png",monthName,idx);
+}
+
+string[] monthName = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+// Getting the list of args from the command line
+string inp;
+
+for (int i = 0; i < 12; i++)
+{
+
+    Console.WriteLine("Please give Date & Time input for " + monthName[i]);
+    for(int j= 0; j < 4; j++)
+    {
+
+        inp = Console.ReadLine();
+        execute(inp,monthName[i],j+1);
+    }
+}
+
+// Assert.IsTrue(result.SequenceEqual(expectedOutput));
+
+//save files into input folder
+
+//helper class method call to show in the console 
+
+
+ArgsConfig config = new ArgsConfig(args);
+
+Experiment ex1 = new Experiment(config);
+ex1.run();
